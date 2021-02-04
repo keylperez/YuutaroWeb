188b791cd3552f1e4a5555eb0b619cc9da9d1096
EjMat8 <franzelijahmatugas@gmail.com>
2021-02-04 12:16:46 +0800

    hello

--- resources/script.js
+++ resources/script.js
@@ -1,17 +1,17 @@
-import {ClipboardJS} from "clipboard";
+import ClipboardJS from "clipboard";
 
-var clipboard = new ClipboardJS('.btn');
+var clipboard = new ClipboardJS(".btn");
 alert("Copied the text: " + copyText.value);
 
-clipboard.on('success', function (e) {
-console.info('Action:', e.action);
-console.info('Text:', e.text);
-console.info('Trigger:', e.trigger);
+clipboard.on("success", function (e) {
+  console.info("Action:", e.action);
+  console.info("Text:", e.text);
+  console.info("Trigger:", e.trigger);
 
-e.clearSelection();
+  e.clearSelection();
 });
 
-clipboard.on('error', function (e) {
-console.error('Action:', e.action);
-console.error('Trigger:', e.trigger);
+clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
 });
